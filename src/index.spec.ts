cd525178ca9c89bdf5ac84dc2eec4c68ec81a3e4
Jean-Baptiste Pionnier <jb.pionnier@gmail.com>
2018-08-27 15:47:29 +0200

    init project

---
+++ src/index.spec.ts
@@ -0,0 +1,32 @@
+import { Env, loadConfig } from './'
+
+export class Config {
+
+  @Env({ required: true })
+  readonly NODE_ENV: string
+
+  @Env('NODE_ENV')
+  readonly ENV: string
+
+  @Env('DEBUG', { required: false })
+  readonly DEBUG: boolean = true
+
+}
+
+describe('env-decorator', () => {
+
+  let config: Config
+
+  beforeAll(() => {
+    config = loadConfig<Config>(Config)
+  })
+
+  test('check config', () => {
+    expect(config).toEqual({
+      NODE_ENV: 'test',
+      ENV: 'test',
+      DEBUG: true,
+    })
+  })
+
+})
